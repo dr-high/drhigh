66c2cb2fb8dafe85a254c8721751cef8927552bf
ericowusu685 <32520113+ericowusu685@users.noreply.github.com>
2024-06-22 01:41:31 +0000

    Delete handlers directory

--- handlers/delivery.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher, types
-
-async def cmd_delivery(message: types.Message):
-    await message.reply("Delivery information...")
-
-def register_handlers_delivery(dp: Dispatcher):
-    dp.register_message_handler(cmd_delivery, commands=['delivery'])
